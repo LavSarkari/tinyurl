e57987e48c0f466338b66db9d6ffe3144fad0a18
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-04 11:55:34 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+pkg install figlet toilet python curl -yecho
+pkg install termux-api
+pip install pyshorteners
+echo This Script Is Created By LavSarkari
+clear
+figlet LavSarkari
+echo -e "\e[1;34m Presents \e[1;32m"
+
+toilet -f mono12 -F border tinyurl
+echo " "
+echo -e "\e[1;34m Your requriments has been installed \e[1;32m"
+termux-tts-speak Congratulations Your Requirements has successfully install.
+termux-tts-speak this script is created by Luv Sarkari.
+termux-tts-speak Now you Can enter the website to shotit.
+python tu.py
